a26f486933eac8c5d60b3ce111212ff5d8f24a1e
Ghan Patel <ghanpatel@gmail.com>
2013-12-25 10:49:00 -0500

    hogan js partials

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Play : Pretty Colors' });
+exports.index = function(req, res) {
+  res.locals = {
+    title: 'Play: Pretty Colors',
+  };
+  return res.render(
+    'index',
+    {
+      partials:
+      {
+        head: 'head',
+        foot: 'foot'
+      }
+    }
+  );
 };
